09841ce6bb773d6ddfec75964b34214c086d00f3
esmeralda <esmeraldateodorosoriano120@gmail.com>
2025-12-09 22:25:43 -0600

    Guardando Cambios

--- Restaurante-main/apps/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'api'
-
-urlpatterns = [
-    path('ordenes-pendientes/', views.OrdenDetalleListAPIView.as_view(), name='orden_detalle_list'),
-]
